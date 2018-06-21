ca22938a66ee6c0da2677fc795c016eb1106859b
Ben woodhouse <bmwoodhouse@hotmail.co.uk>
2018-06-21 21:00:35 +0100

    Added controllers, DB connections and Models

---
+++ RegistrationApplication/Models/UserDBHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace RegistrationApplication.Models
+{
+    public class UserDBHandler
+    {
+
+        private SqlConnection sqlConnection;
+        private void SqlConnector()
+        {
+            var sqlConnectionString = ConfigurationManager.ConnectionStrings["UserConnection"].ToString();
+            sqlConnection = new SqlConnection(sqlConnectionString);
+        }
+    }
+}
